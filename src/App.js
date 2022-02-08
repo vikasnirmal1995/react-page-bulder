65bb14966188e8ae1763a2532e9a66f3e94befc4
vikasnirmal1995 <vikasnirmal1995@gmail.com>
2022-02-08 20:51:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import 'grapesjs/dist/css/grapes.min.css';
+import grapesjs from 'grapesjs';
+import gjsPresetWebpage from 'grapesjs-preset-webpage'
+import "./styles/main.scss"
+const  App = () => {
+    const [editor, setEditor] = useState(null)
 
-function App() {
+    console.log("editor states>>", editor.getHtml())
+
+    useEffect(() =>{
+        const editor = grapesjs.init({
+            container: "#editor",
+            plugins: [gjsPresetWebpage],
+            pluginsOpts:{
+                gjsPresetWebpage: {}
+            }
+        });
+        setEditor(editor)
+    },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={"editor"} id={"editor"}></div>
     </div>
   );
 }
